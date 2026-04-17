957c3efcfa9c8ae4a7cd6998996e9c6f78040720
Alonso <alonsoherrera847@gmail.com>
2026-04-16 22:56:51 -0600

    barra buscadora BELLAKEANDO LE METO EL P*NE TRAAAAA

--- Front/src/app/buscador/buscador.ts
+++ Front/src/app/buscador/buscador.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-buscador',
-  standalone: true,
-  imports: [],
-  templateUrl: './buscador.html',
-  styleUrl: './buscador.css',
+  template: `<h1>Resultados para: {{ terminoBusqueda }}</h1>`
 })
-export class Buscador {}
+export class BuscadorComponent implements OnInit {
+  terminoBusqueda: string = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Escuchamos los cambios en los parámetros de la URL
+    this.route.queryParams.subscribe(params => {
+      this.terminoBusqueda = params['q'];
+      this.realizarBusqueda(this.terminoBusqueda);
+    });
+  }
+
+  realizarBusqueda(termino: string) {
+    // Aquí es donde llamarías a tu servicio de música o letras
+    console.log('Buscando en la base de datos:', termino);
+  }
+}
