cac01f9c8ebdce529c66cc9d1d7bcc6c6f0ea590
Diana Ruth <llol7452@gmail.com>
2026-04-17 19:41:32 -0600

    comentario

--- Front/src/app/app.routes.server.ts
+++ Front/src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'detalle/:id',
+    renderMode: RenderMode.Server  // dinámico, se renderiza por petición
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender  // el resto se prerenderiza sin problema
   }
 ];
